14aeb0965ca6c10f73ec5fdb5388e13e057ed6e8
ytkinroman <mc3heisenberg@gmail.com>
2025-05-09 20:18:40 +0500

    add hotkey

---
+++ hotkey_listener.py
@@ -0,0 +1,15 @@
+
+# This is a comment.
+
+
+def welcome_message(message: str) -> None:
+    print(message)
+
+
+def main() -> None:
+    message = "Hi."
+    welcome_message(message)
+
+
+if __name__ == "__main__":
+    main()
