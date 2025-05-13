b72b164f8a531769187eebf5e55cab70b84934dc
ytkinroman <mc3heisenberg@gmail.com>
2025-05-13 19:50:01 +0500

    Upd key_listener

--- key_listener.py
+++ key_listener.py
@@ -2,12 +2,10 @@ import json
 from logging import getLogger
 import keyboard
 import pyperclip
-from threading import Thread, Lock
-import time
+from threading import Thread
 from config_manager import ConfigurationManager
 import websocket
 import requests
-
 
 logger = getLogger(__name__)
 
@@ -15,21 +13,20 @@ logger = getLogger(__name__)
 class KeyListener(Thread):
     def __init__(self, config_manager: ConfigurationManager):
         super().__init__(daemon=True)
-        self.__config_manager = config_manager
-        self.__key_combination = self.__config_manager.get_hotkey()  # например "alt+shift+t"
+        self.__config = config_manager
+        self.__key_combination = self.__config.user.translate_keyboard  # например "alt+shift+t"
         self.__running = True
         logger.info(f"Инициализация KeyListener с комбинацией {self.__key_combination}")
         self.ws = websocket.WebSocket()
 
-    def run(self):
+    def start(self):
         """Метод, который запускается при старте потока"""
         logger.info("Запуск отслеживания комбинации клавиш")
-        
-        
-        self.ws.connect("ws://212.220.211.93:8081/ws")
+
+        self.ws.connect(self.__config.server.websocket_url)
         session_id = json.loads(self.ws.recv()).get("session_id")
         logger.info(f"Получен session_id: {session_id}")
-        
+
         def on_hotkey_press():
             """Обработчик нажатия комбинации клавиш"""
             logger.info(f"Обнаружено нажатие комбинации {self.__key_combination}")
@@ -39,23 +36,23 @@ class KeyListener(Thread):
                 clipboard_text = pyperclip.paste()
                 trimmed_text = clipboard_text[:900] if clipboard_text else ""
 
-                #print(trimmed_text)
+                # print(trimmed_text)
                 session = requests.Session()
                 response = session.post(
-                    url="http://212.220.211.93:8081/translate", 
+                    url=self.__config.server.translate_api_url,
                     json={
-                        "method": "translate", 
-                        "ws_session_id": session_id, 
+                        "method": "translate",
+                        "ws_session_id": session_id,
                         "payload": {
-                            "text": trimmed_text, 
-                            "translator_code": "yandex",#self.__config_manager.get_translator().get_code(),
-                            "target_lang": "ru",#self.__config_manager.get_language().get_code(),
+                            "text": trimmed_text,
+                            "translator_code": self.__config.user.selected_translator,  # self.__config_manager.get_translator().get_code(),
+                            "target_lang": self.__config.user.selected_language,  # self.__config_manager.get_language().get_code(),
                         }
                     }
                 )
-                
+
                 print(response.json())
-                
+
                 if response.status_code != 200:
                     logger.error(f"Ошибка при отправке запроса: {response.status_code} - {response.text}")
                     return
