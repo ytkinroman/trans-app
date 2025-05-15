59062d98c23ff68c1c61fc0626960521f40b6690
ytkinroman <mc3heisenberg@gmail.com>
2025-05-15 16:44:11 +0500

    Update utils.py
    - Add get_config_dir()

--- module/utils.py
+++ module/utils.py
@@ -1,5 +1,7 @@
 from PIL import Image, ImageDraw
 from logging import getLogger
+import os
+import sys
 
 
 logger = getLogger(__name__)
@@ -12,3 +14,14 @@ def create_app_icon() -> Image:
     dc.ellipse((padding, padding, width - padding, height - padding), fill="yellow")
     logger.info("Application icon initialized successfully")
     return image
+
+def get_config_dir() -> str:
+    if getattr(sys, 'frozen', False):
+        base_dir = os.path.dirname(sys.executable)
+    else:
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+
+    config_dir = os.path.join(base_dir, "config")
+    os.makedirs(config_dir, exist_ok=True)
+
+    return config_dir
